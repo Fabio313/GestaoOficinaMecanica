c132282c8ad6f9434bff25023c57528b13cbe222
Fabio313 <fabiozf3108@gmail.com>
2025-08-28 21:12:00 -0300

    feat: criacao de arquivos de deploy, e configuracao de ingress

--- gestao-site/src/api/httpClient.ts
+++ gestao-site/src/api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://localhost:44308/api",
+  baseURL: "http://gestao.mecanica/api",
   headers: {
     "Content-Type": "application/json",
   },
